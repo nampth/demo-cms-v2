d2da1626ffe4f16fe5833457277f93246e1f6a47
Nam Phan <nampth@Nams-MacBook-Air.local>
2022-08-01 09:58:11 +0700

    add laradock

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->nullable();
